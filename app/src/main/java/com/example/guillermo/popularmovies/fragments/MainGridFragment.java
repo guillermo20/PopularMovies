984de5a7b1fd3c2fd6ca8e0698b79ea65b97db3a
Guillermo <guillermo20@gmail.com>
2016-09-11 15:46:30 -0430

    fetching data from the movie db server

--- app/src/main/java/com/example/guillermo/popularmovies/fragments/MainGridFragment.java
+++ app/src/main/java/com/example/guillermo/popularmovies/fragments/MainGridFragment.java
@@ -2,13 +2,14 @@ package com.example.guillermo.popularmovies.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.guillermo.popularmovies.R;
+
+import java.util.List;
 
 /**
  * Created by guillermo on 11/09/16.
@@ -17,11 +18,32 @@ public class MainGridFragment extends Fragment{
 
     private final String LOG_TAG = MainGridFragment.class.getSimpleName();
 
-    @Nullable
+    private List<String> popularMoviesList;
+    private List<String> mostRatedMoviesList;
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Log.v(LOG_TAG,"maingrid oncreateview");
         View root = inflater.inflate(R.layout.main_grid_fragment, container,false);
         return root;
     }
+
+    private List<String> getPopularMoviesData(){
+        return null;
+    }
+
+    private List<String> getTopRatedMoviesData(){
+        return null;
+    }
+
 }
