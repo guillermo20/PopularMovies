b08ccf14824c091c58c59e659f0fd650615a430e
Guillermo <guillermo20@gmail.com>
2016-09-11 21:45:26 -0430

    added necesary code to validate that the data is well received

--- app/src/main/java/com/example/guillermo/popularmovies/fragments/MainGridFragment.java
+++ app/src/main/java/com/example/guillermo/popularmovies/fragments/MainGridFragment.java
@@ -9,41 +9,40 @@ import android.view.ViewGroup;
 
 import com.example.guillermo.popularmovies.R;
 import com.example.guillermo.popularmovies.backgroundtasks.FetchPopularMoviesTask;
-
-import java.util.List;
+import com.example.guillermo.popularmovies.model.MovieItem;
 
 /**
  * Created by guillermo on 11/09/16.
  */
-public class MainGridFragment extends Fragment{
+public class MainGridFragment extends Fragment {
 
     private final String LOG_TAG = MainGridFragment.class.getSimpleName();
 
-    private List<String> popularMoviesList;
+    /*private List<String> popularMoviesList;
     private List<String> mostRatedMoviesList;
-
-
+    */
+    private FetchPopularMoviesTask backgroundTask;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        new FetchPopularMoviesTask().execute();
+        if (backgroundTask == null) {
+            backgroundTask = new FetchPopularMoviesTask();
+        }
+        backgroundTask.execute();
         super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Log.v(LOG_TAG,"maingrid oncreateview");
-        View root = inflater.inflate(R.layout.main_grid_fragment, container,false);
+        Log.v(LOG_TAG, "maingrid oncreateview");
+        View root = inflater.inflate(R.layout.main_grid_fragment, container, false);
+        if (backgroundTask.getListMovieItem() != null) {
+            for (MovieItem item : backgroundTask.getListMovieItem()) {
+                Log.v(LOG_TAG, "Item =" + item.getTitle());
+            }
+        }
         return root;
-    }
-
-    private List<String> getPopularMoviesData(){
-        return null;
-    }
-
-    private List<String> getTopRatedMoviesData(){
-        return null;
     }
 
 }
