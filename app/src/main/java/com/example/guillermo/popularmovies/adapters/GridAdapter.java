07556f28c3af3b032fb63610aecc814116459224
Guillermo <guillermo20@gmail.com>
2016-09-13 21:35:27 -0430

    fixed the reference to the imageview so it can display the posters on the gridview

--- app/src/main/java/com/example/guillermo/popularmovies/adapters/GridAdapter.java
+++ app/src/main/java/com/example/guillermo/popularmovies/adapters/GridAdapter.java
@@ -1,14 +1,12 @@
 package com.example.guillermo.popularmovies.adapters;
 
-import android.app.Activity;
 import android.content.Context;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 
-import com.example.guillermo.popularmovies.R;
 import com.example.guillermo.popularmovies.model.MovieItem;
 import com.squareup.picasso.Picasso;
 
@@ -21,30 +19,27 @@ public class GridAdapter extends ArrayAdapter<MovieItem> {
 
     private Context context;
 
+    private final String LOG_TAG = GridAdapter.class.getSimpleName();
+
     public GridAdapter(Context context, int resource, List<MovieItem> objects) {
         super(context, resource, objects);
         this.context = context;
     }
-    private class ViewHolder {
-        ImageView imageView;
-    }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder = null;
+        ImageView imageView = null;
         MovieItem movieItem = getItem(position);
-
-        LayoutInflater mInflater = (LayoutInflater) context
-                .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
         if (convertView == null) {
-            convertView = mInflater.inflate(R.layout.main_grid_fragment, null);
-            holder = new ViewHolder();
-            holder.imageView = new ImageView(context);
-            convertView.setTag(holder);
-        } else
-            holder = (ViewHolder) convertView.getTag();
-        Picasso.with(context).load(movieItem.getThumbUri()).noFade().into(holder.imageView);
-
-        return convertView;
+            imageView = new ImageView(context);
+        } else{
+            imageView = (ImageView) convertView;
+        }
+        Log.v(LOG_TAG,""+movieItem.getThumbUri());
+        Picasso.with(context)
+                .load(movieItem.getThumbUri())
+                .resize(400,540).centerInside()
+                .into(imageView);
+        return imageView;
     }
 }
